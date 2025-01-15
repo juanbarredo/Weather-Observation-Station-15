7bc958197d4286ea40c8a22d87908a06db01775b
juanbarredo <83099605+juanbarredo@users.noreply.github.com>
2025-01-14 20:26:22 -0500

    01 14 2025
    I guess I am going on a side-quest to correct the title of a column in STATION table.
    
    I am trying to change LON_N TO LONG_W.

---
+++ DROPING STATION to RE-UPLOAD WITH CORRECT COLUMN TITLES side quest.sql
@@ -0,0 +1,19 @@
+--ok, 
+--I need to plan this pomodoro
+--I need to change the name of the column (LON_N) to (LON_W) in the STATION table
+--this means that I have to errase the table.
+
+--and re-upload it.
+--re-upload it using the import/export wizard.
+
+--let me look up how to delete a table.
+
+USE Weather_Observation_Station_2;
+DROP TABLE STATION;
+
+
+--Ok,
+--I failed at uploading a new table with the correct name
+--but the column title I need to change is LON_N to LONG_W.
+--very different.
+
